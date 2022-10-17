fcce003922408627eacda5ef983e9c4a2209a862
pranayselokar <pranayselokar2@gmail.com>
2022-10-17 19:59:39 +0530

    implemented helloworld project

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        /// <summary>
+        /// using to print method 1
+        /// </summary>
+        public static void Method1()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Method1();
+          
+        }
+    }
+}
