7bd17846e46b0b77512bb4380cb7e70afbc6940e
pranayselokar <pranayselokar2@gmail.com>
2022-10-17 20:05:09 +0530

    added second line of code

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -14,6 +14,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Evening");
             Method1();
           
         }
